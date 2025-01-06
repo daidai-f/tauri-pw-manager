e88c18fe0b3769e402493316b1876fab186268e7
daidai-f <daidai127001@gmail.com>
2025-01-07 00:35:34 +0900

    feat: Add backend function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,38 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![login, logout, create_account, window_close, window_minimize, window_toggle_fullscreen])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn login(username: String, password: String) -> Result<(), String> {
+    Ok(())
+}
+
+#[tauri::command]
+async fn logout() -> Result<(), String> {
+    Ok(())
+}
+
+#[tauri::command]
+async fn create_account(username: String, password: String) -> Result<(), String> {
+    Ok(())
+}
+
+#[tauri::command]
+async fn window_close() -> Result<(), String> {
+    Ok(())
+}
+
+#[tauri::command]
+async fn window_minimize() -> Result<(), String> {
+    Ok(())
+}
+
+#[tauri::command]
+async fn window_toggle_fullscreen() -> Result<(), String> {
+    Ok(())
 }
